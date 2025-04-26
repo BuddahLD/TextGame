68de4633d199656fbb164c9c2b54e7f0c4f1674e
Vladick King <nanololki123@gmail.com>
2025-04-26 21:49:34 +0300

    All movement + ByeBye colour key + border colour

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,8 +6,6 @@ enum class AllowedKey {
 }
 
 fun main() {
-    val reset = "\u001B[0m"
-    val red = "\u001B[31m"
     val width = 10
     val height = 10
     val map = MutableList(height) { MutableList(width) { "[ ]" } }
@@ -28,6 +26,8 @@ fun main() {
 
         when (pressedKey) {
             AllowedKey.W -> {
+                val reset = "\u001B[0m"
+                val red = "\u001B[31m"
                 val newPositionY = posY - 1
                 if (newPositionY >= 0) {
                     map[posY][posX] = "[ ]"
@@ -41,6 +41,8 @@ fun main() {
             }
 
             AllowedKey.A -> {
+                val reset = "\u001B[0m"
+                val red = "\u001B[31m"
                 val newPositionX = posX - 1
                 if (newPositionX >= 0) {
                     map[posY][posX] = "[ ]"
@@ -55,6 +57,8 @@ fun main() {
             }
 
             AllowedKey.S -> {
+                val reset = "\u001B[0m"
+                val red = "\u001B[31m"
                 val newPositionY = posY + 1
                 if (newPositionY <= 9) {
                     map[posY][posX] = "[ ]"
@@ -68,6 +72,8 @@ fun main() {
             }
 
             AllowedKey.D -> {
+                val reset = "\u001B[0m"
+                val red = "\u001B[31m"
                 val newPositionX = posX + 1
                 if (newPositionX <= 9) {
                     map[posY][posX] = "[ ]"
@@ -81,7 +87,14 @@ fun main() {
             }
 
             AllowedKey.Q -> {
-                println("ByEbYe")
+                val reset = "\u001B[0m"
+                val red = "\u001B[31m"
+                val yellow = "\u001B[33m"
+                val green = "\u001B[32m"
+                val cyan = "\u001B[36m"
+                val blue = "\u001B[34m"
+                val purple = "\u001B[35m"
+                println("${red}B${yellow}Y${green}E${cyan}B${blue}Y${purple}E${red}E$reset")
 
             }
 
